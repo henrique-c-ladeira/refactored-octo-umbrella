6f723ba797f374c83e41154df947f86d8ad187ec
Henrique Curzio Ladeira <henrique.c.ladeira@gmail.com>
2022-04-06 14:59:06 -0300

    refactor: change fetch to axios

--- src/services/fetchPokemonList.ts
+++ src/services/fetchPokemonList.ts
@@ -1,10 +1,20 @@
+import axios from 'axios';
+
+type DataFetchType = {
+  count: number;
+  next?: string;
+  previous?: string;
+  results: PokemonListReponse[];
+};
+
 type PokemonListReponse = {
   name: string;
   url: string;
 };
 
 export const fetchPokemonList: () => Promise<string[]> = async () => {
-  const response = await fetch('https://pokeapi.co/api/v2/pokemon');
-  const responseJson = await response.json();
-  return responseJson.results.map((item: PokemonListReponse) => item.name);
+  const { data } = await axios.get<DataFetchType>(
+    'https://pokeapi.co/api/v2/pokemon?limit=400',
+  );
+  return data.results.map((item: PokemonListReponse) => item.name);
 };
