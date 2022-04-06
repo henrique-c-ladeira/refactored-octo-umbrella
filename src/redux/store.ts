99128af046830ab4f7175b42674f84c52fb39c9d
Henrique Curzio Ladeira <henrique.c.ladeira@gmail.com>
2022-04-06 18:29:48 -0300

    refactor: move services folder

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,7 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
 // Or from '@reduxjs/toolkit/query/react'
 import { setupListeners } from '@reduxjs/toolkit/query';
-import { pokemonApi } from './services/pokemonApi/pokemonApi';
+import { pokemonApi } from '../services/pokemonApi/pokemonApi';
 
 export const store = configureStore({
   reducer: {
