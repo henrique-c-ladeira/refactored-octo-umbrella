dcee04b063f16de99c325b39f5008f8f42153bc9
Henrique Curzio Ladeira <henrique.c.ladeira@gmail.com>
2022-04-07 17:56:09 -0300

    feat: add pokemon details screen

--- src/screens/PokemonDetaiils.tsx
+++ src/screens/PokemonDetaiils.tsx
@@ -1,17 +1,60 @@
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import React from 'react';
-import { Text } from 'react-native';
+import {
+  ActivityIndicator,
+  Image,
+  ScrollView,
+  StyleSheet,
+  Text,
+} from 'react-native';
 import { RootStackParamList } from '../Routes';
+import { useGetPokemonDetailsQuery } from '../services/pokemonApi';
 
 type PokemonDetailsProps = NativeStackScreenProps<
   RootStackParamList,
   'PokemonDetails'
 >;
 
-export const PokemonDetails: React.FC<PokemonDetailsProps> = () => {
+export const PokemonDetails: React.FC<PokemonDetailsProps> = ({ route }) => {
+  const pokemonName = route.params.headerTitle;
+  const { data, isError, isFetching, isSuccess } =
+    useGetPokemonDetailsQuery(pokemonName);
+
   return (
-    <>
-      <Text style={{ color: 'black' }}>TODO</Text>
-    </>
+    <ScrollView
+      style={styles.background}
+      contentContainerStyle={styles.screenContainer}>
+      {isFetching && <ActivityIndicator />}
+
+      {isSuccess && (
+        <>
+          <Image source={{ uri: data.imageUrl }} style={styles.image} />
+          <Text>Type</Text>
+          {data.types.map(item => (
+            <Text>{item}</Text>
+          ))}
+          <Text>Stats</Text>
+          {data.stats.map(item => (
+            <>
+              <Text>{item.name}</Text>
+              <Text>{item.value}</Text>
+            </>
+          ))}
+        </>
+      )}
+
+      {isError && <Text>Something unnexpected's happenned. </Text>}
+    </ScrollView>
   );
 };
+
+const styles = StyleSheet.create({
+  background: {
+    backgroundColor: 'black',
+  },
+  screenContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  image: { width: '100%', height: 240, resizeMode: 'contain' },
+});
